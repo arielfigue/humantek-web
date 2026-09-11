175a71fc281cf9db7152858066ff2272c8a4d5df
humanytek <ariel@humanytek.com>
2026-09-10 18:36:06 -0600

    quitar industria 4.0

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,6 @@ export default function Footer() {
     { name: "ERP's", href: "/erps" },
     { name: "Metodología", href: "/metodologia" },
     { name: "Casos de éxito", href: "/casos-de-exito" },
-    { name: "Industria 4.0", href: "/#industria-40" },
     { name: "VMI", href: "/vendor-managed-inventory" },
     { name: "Inteligencia Artificial", href: "/inteligencia-artificial" },
     { name: "Nosotros", href: "/#nosotros" },
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,6 @@ export default function Navbar() {
     { name: "ERP's", href: "/erps" },
     { name: "Metodología", href: "/metodologia" },
     { name: "Casos de éxito", href: "/casos-de-exito" },
-    { name: "Industria 4.0", href: "/#industria-40" },
     { name: "VMI", href: "/vendor-managed-inventory" },
     { name: "Inteligencia Artificial", href: "/inteligencia-artificial" },
     { name: "Nosotros", href: "/#nosotros" },
